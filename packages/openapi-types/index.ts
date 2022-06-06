8b07da12a7530533fcdee071c64b0ba92ef4696d
Eugene Leonov <yauheni.liavonau@fabros-team.com>
2022-06-06 18:40:52 +0200

    Update openapi-types v3 interface

--- packages/openapi-types/index.ts
+++ packages/openapi-types/index.ts
@@ -596,7 +596,7 @@ export namespace OpenAPIV3_1 {
 
 export namespace OpenAPIV3 {
   export interface Document<T extends {} = {}> {
-    openapi: string;
+    configapi: string;
     info: InfoObject;
     servers?: ServerObject[];
     paths: PathsObject<T>;
